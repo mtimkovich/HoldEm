f31e14e4e624be39a9c7c7c60390e713b120927c
Max Timkovich <mtimkovich@fisoc.com>
2013-07-31 15:27:01 -0500

    Moved hand ranking to its own file

--- HoldEm.java
+++ HoldEm.java
@@ -27,69 +27,6 @@ class Player {
 }
 
 public class HoldEm {
-    public static Hand containsStreak(ArrayList<Card> cards, int length) {
-        ArrayList<Card> streak = new ArrayList<Card>();
-
-        for (int i = 0; i < cards.size()-1; i++) {
-            Card next = cards.get(i+1);
-
-            if (cards.get(i).getRank().equals(next.getRank())) {
-                if (streak.isEmpty()) {
-                    streak.add(cards.get(i));
-                }
-
-                streak.add(next);
-            } else {
-                streak.clear();
-            }
-
-            if (streak.size() == length) {
-                Hand hand = new Hand();
-
-                hand.setHandStreak(length);
-
-                hand.addAll(streak);
-
-                for (Card card : cards) {
-                    if (hand.getCards().size() == 5) {
-                        break;
-                    }
-
-                    if (! hand.getCards().contains(card)) {
-                        hand.add(card);
-                    }
-                }
-
-                return hand;
-            }
-        }
-
-        return null;
-    }
-
-    public static Hand rankHand(ArrayList<Card> player, ArrayList<Card> community) {
-        ArrayList<Card> cards = new ArrayList<Card>();
-
-        cards.addAll(player);
-        cards.addAll(community);
-
-        Collections.sort(cards, Collections.reverseOrder());
-
-        System.out.println(cards);
-        Hand hand = null;
-
-        for (int i = 4; i >= 2; i--) {
-            hand = containsStreak(cards, i);
-
-            if (hand != null) {
-                System.out.println(hand);
-                break;
-            }
-        }
-
-
-        return hand;
-    }
 
     public static void main(String[] args) {
         Deck deck = new Deck();
@@ -130,7 +67,8 @@ public class HoldEm {
 //         System.out.println(community);
 //         System.out.println(players.get(0));
 
-        rankHand(players.get(0).getHand(), community);
+        RankHand rankHand = new RankHand();
+        rankHand.rank(players.get(0).getHand(), community);
         
 //         for (int i = 0; i < PLAYER_COUNT; i++) {
 //             System.out.println(players.get(i));
---
+++ cards/RankHand.java
@@ -0,0 +1,69 @@
+package cards;
+
+import java.util.*;
+
+public class RankHand {
+    public static Hand containsStreak(ArrayList<Card> cards, int length) {
+        ArrayList<Card> streak = new ArrayList<Card>();
+
+        for (int i = 0; i < cards.size()-1; i++) {
+            Card next = cards.get(i+1);
+
+            if (cards.get(i).getRank().equals(next.getRank())) {
+                if (streak.isEmpty()) {
+                    streak.add(cards.get(i));
+                }
+
+                streak.add(next);
+            } else {
+                streak.clear();
+            }
+
+            if (streak.size() == length) {
+                Hand hand = new Hand();
+
+                hand.setHandStreak(length);
+
+                hand.addAll(streak);
+
+                for (Card card : cards) {
+                    if (hand.getCards().size() == 5) {
+                        break;
+                    }
+
+                    if (! hand.getCards().contains(card)) {
+                        hand.add(card);
+                    }
+                }
+
+                return hand;
+            }
+        }
+
+        return null;
+    }
+
+    public static Hand rank(ArrayList<Card> player, ArrayList<Card> community) {
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        cards.addAll(player);
+        cards.addAll(community);
+
+        Collections.sort(cards, Collections.reverseOrder());
+
+        System.out.println(cards);
+        Hand hand = null;
+
+        for (int i = 4; i >= 2; i--) {
+            hand = containsStreak(cards, i);
+
+            if (hand != null) {
+                System.out.println(hand);
+                break;
+            }
+        }
+
+
+        return hand;
+    }
+}
